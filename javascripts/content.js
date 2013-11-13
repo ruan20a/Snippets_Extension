875633de57f24dcd72ab3874cbebdbe08da673a7
Amy Ruan <nyc.amy@gmail.com>
2013-11-12 23:48:20 -0500

    view and post from form good to go...but in the midst of fixing the way i grab the text so user can highlight backward and forward (using anchors and focus)

--- javascripts/content.js
+++ javascripts/content.js
@@ -3,18 +3,27 @@
 $(document).ready(function(){
     var new_data = null;
   $(document).on("mouseup",function(e){
+
+    //TODO testing for links!! please don't delete
     console.log(window.getSelection());
     console.log(e);
-
+    //TODO need to differentiate between text and links
     var fromDOM = window.getSelection();
-
-    //startNum and endNum figures out the text
-    var startNum = fromDOM.baseOffset;
-    var endNum = fromDOM.extentOffset - startNum;
+    //anchor = where you mouse down
+    var anchor = fromDOM.anchorOffset;
+    //focus = where you mouse up
+    var focus = fromDOM.focusOffset;
+    //gets the data
     var data = fromDOM.baseNode.data;
+    //gets the source
+    var new_source = fromDOM.baseNode.baseURI;
+    //figures out the text highlighted
+
+
+
     new_data = data.substr(startNum,endNum);
-    // alert(new_data)
-    chrome.runtime.sendMessage({method:'setTitle', title: new_data});
+    //sets the chrome data
+    chrome.runtime.sendMessage({method:'setTitle', title: new_data, source: new_source});
   })
 
     // alert(new_data + "hey")
--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,6 +1,62 @@
+var Snippet = Snippet || {};
+
 $( document ).ready(function(){
+  $('#show-snippet').on("click", Snippet.showSnippets)
+  $('#post-snippet').on("submit", Snippet.addSnippets)
+  //reseting the values so it doesnt automatically take the last updated value
+  $('input[name=body]').val("")
+  $('input[name=source]').val("")
+  //calling for my background page
   var bg = chrome.extension.getBackgroundPage();
-  console.log(bg.title)
-  $('#testing').text(bg.title)
+  $('input[name=body]').val(bg.title)
+  $('input[name=source]').val(bg.source)
   }
 );
+
+Snippet.showSnippets = function() {
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:3000/snippets.json",
+    crossDomain: true,
+    dataType: "json"
+  }).done(function(snippets){
+    var $ul = $("<ul>");
+    $.each(snippets, function(index, snippet){
+      $snippet = $("<li>")
+        .text(snippet.body)
+        .appendTo($ul);
+    })
+    $ul.appendTo($('#all-snippets'))
+  });
+   $('#show-snippet').off("click", Snippet.showSnippets)
+  }
+
+Snippet.addSnippets = function(e){
+  e.preventDefault();
+    var newSnippet = {
+      body: $('input[name=body]').val(),
+      source: $('input[name=source]').val(),
+      user_id: $('input[name=user_id]').val(),
+      tag_list: $('input[name=tag_list]').val()
+    };
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:3000/snippets.json",
+      dataType: "json",
+      crossDomain: true,
+      data: {snippet: newSnippet}
+    }).done(function(snippet){
+      alert("hello")
+      console.log(snippet.tag_list)})
+}
+
+Snippet.deleteSnippets = function(e){
+  e.preventDefault();
+  var $target = $(e.target).parent();
+  var snippet_id = $target.attr('id')
+      $.ajax({
+      type: "DELETE",
+      url: "/snippets/"+ snippet_id,
+      dataType: "json"
+    })
+}
